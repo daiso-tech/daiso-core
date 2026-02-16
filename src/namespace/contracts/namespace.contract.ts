9b4fd5c78f2f83d6cb56dce762cb425d5fe78d48
Yousif <yousif@fillerdepot.com>
2026-02-16 01:04:15 +0100

    Added source code documentation to INamepsace contract

--- src/namespace/contracts/namespace.contract.ts
+++ src/namespace/contracts/namespace.contract.ts
@@ -5,19 +5,35 @@
 import { type IEquals } from "@/utilities/_module.js";
 
 /**
+ * The `IKey` contracts represents namespacable key.
  * @group Contracts
  */
 export interface IKey extends IEquals<IKey> {
+    /**
+     * The `get` method returns the key without namespace
+     */
     get(): string;
 
+    /**
+     * The `toString` method returns the key with namespace.
+     */
     toString(): string;
 }
 
 /**
+ * The `INamespace` contract is used for easily namespacing.
+ * Note this class used by other component in this library.
+ *
  * @group Contracts
  */
 export type INamespace = {
+    /**
+     * The `toString` method returns the namespace prefix.
+     */
     toString(): string;
 
+    /**
+     * The `create` method returns {@link IKey | `IKey`} object.
+     */
     create(key: string): IKey;
 };
