5fade0f8ea58f230edbf1e61d5e0e1f89c5776a6
Yousif <yousif@fillerdepot.com>
2026-03-18 01:38:11 +0100

    Added NoOpFileStorageAdapter class

---
+++ src/file-storage/implementations/adapters/no-op-file-storage-adapter/_module-exports.ts
@@ -0,0 +1,1 @@
+export * from "@/file-storage/implementations/adapters/no-op-file-storage-adapter/no-op-file-storage-adapter.js";
---
+++ src/file-storage/implementations/adapters/no-op-file-storage-adapter/no-op-file-storage-adapter.ts
@@ -0,0 +1,117 @@
+/**
+ * @module FileStorage
+ */
+
+import {
+    FILE_WRITE_ENUM,
+    type ISignedFileStorageAdapter,
+    type FileAdapterMetadata,
+    type FileAdapterStream,
+    type FileWriteEnum,
+    type WritableFileAdapterContent,
+    type WritableFileAdapterStream,
+    type FileAdapterSignedDownloadUrlSettings,
+    type FileAdapterSignedUploadUrlSettings,
+} from "@/file-storage/contracts/_module.js";
+
+/**
+ * The `NoOpFileStorageAdapter` will do nothing and is used for easily mocking {@link ISignedFileStorageAdapter | `ISignedFileStorageAdapter`} for testing.
+ *
+ * IMPORT_PATH: `"@daiso-tech/core/file-storage/no-op-file-storage-adapter"`
+ * @group Adapters
+ */
+export class NoOpFileStorageAdapter implements ISignedFileStorageAdapter {
+    getPublicUrl(_key: string): Promise<string | null> {
+        return Promise.resolve(null);
+    }
+
+    getSignedDownloadUrl(
+        _key: string,
+        _settings: FileAdapterSignedDownloadUrlSettings,
+    ): Promise<string | null> {
+        return Promise.resolve(null);
+    }
+
+    getSignedUploadUrl(
+        _key: string,
+        _settings: FileAdapterSignedUploadUrlSettings,
+    ): Promise<string> {
+        return Promise.resolve("");
+    }
+
+    exists(_key: string): Promise<boolean> {
+        return Promise.resolve(false);
+    }
+
+    getStream(_key: string): Promise<FileAdapterStream | null> {
+        return Promise.resolve(null);
+    }
+
+    getBytes(_key: string): Promise<Uint8Array | null> {
+        return Promise.resolve(null);
+    }
+
+    getMetaData(_key: string): Promise<FileAdapterMetadata | null> {
+        return Promise.resolve(null);
+    }
+
+    add(_key: string, _content: WritableFileAdapterContent): Promise<boolean> {
+        return Promise.resolve(true);
+    }
+
+    addStream(
+        _key: string,
+        _stream: WritableFileAdapterStream,
+    ): Promise<boolean> {
+        return Promise.resolve(true);
+    }
+
+    update(
+        _key: string,
+        _content: WritableFileAdapterContent,
+    ): Promise<boolean> {
+        return Promise.resolve(true);
+    }
+
+    updateStream(
+        _key: string,
+        _stream: WritableFileAdapterStream,
+    ): Promise<boolean> {
+        return Promise.resolve(true);
+    }
+
+    put(_key: string, _content: WritableFileAdapterContent): Promise<boolean> {
+        return Promise.resolve(true);
+    }
+
+    putStream(
+        _key: string,
+        _stream: WritableFileAdapterStream,
+    ): Promise<boolean> {
+        return Promise.resolve(true);
+    }
+
+    copy(_source: string, _destination: string): Promise<FileWriteEnum> {
+        return Promise.resolve(FILE_WRITE_ENUM.SUCCESS);
+    }
+
+    copyAndReplace(_source: string, _destination: string): Promise<boolean> {
+        return Promise.resolve(true);
+    }
+
+    move(_source: string, _destination: string): Promise<FileWriteEnum> {
+        return Promise.resolve(FILE_WRITE_ENUM.SUCCESS);
+    }
+
+    moveAndReplace(_source: string, _destination: string): Promise<boolean> {
+        return Promise.resolve(true);
+    }
+
+    removeMany(_keys: Array<string>): Promise<boolean> {
+        return Promise.resolve(true);
+    }
+
+    removeByPrefix(_prefix: string): Promise<void> {
+        return Promise.resolve();
+    }
+}
