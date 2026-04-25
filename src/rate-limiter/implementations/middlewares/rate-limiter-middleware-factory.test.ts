f99c504334644b497d31958340084691044f66e1
Yousif <yousif@fillerdepot.com>
2026-04-25 19:19:01 +0200

    Added rateLimiterMiddlewareFactory

---
+++ src/rate-limiter/implementations/middlewares/rate-limiter-middleware-factory.test.ts
@@ -0,0 +1,5 @@
+import { describe, test } from "vitest";
+
+describe("function: rateLimiterMiddlewareFactory", () => {
+    test.todo("Write tests!!!");
+});
---
+++ src/rate-limiter/implementations/middlewares/rate-limiter-middleware-factory.ts
@@ -0,0 +1,47 @@
+/**
+ * @module RateLimiter
+ */
+
+import { type MiddlewareFn } from "@/middleware/types.js";
+import { type IRateLimiterFactoryBase } from "@/rate-limiter/contracts/_module.js";
+import {
+    callInvokable,
+    type ErrorPolicy,
+    type Invokable,
+} from "@/utilities/_module.js";
+
+/**
+ * @group Middleware
+ */
+export type RateLimiterMiddlewareSettings<
+    TParameters extends Array<unknown> = Array<unknown>,
+> = ErrorPolicy & {
+    /**
+     * @default
+     * ```ts
+     * (...args) => JSON.stringify(args)
+     * ```
+     */
+    key?: Invokable<TParameters, string>;
+    onlyError?: boolean;
+    limit: number;
+};
+
+/**
+ * IMPORT_PATH: `"@daiso-tech/core/rate-limiter/middlewares"`
+ * @group Middleware
+ */
+export function rateLimiterMiddlewareFactory(
+    rateLimiterFactory: IRateLimiterFactoryBase,
+) {
+    return <TParameters extends Array<unknown>, TReturn>(
+        settings: RateLimiterMiddlewareSettings<TParameters>,
+    ): MiddlewareFn<TParameters, Promise<TReturn>> => {
+        const { key = (...args) => JSON.stringify(args), ...rest } = settings;
+        return ({ next, args }) => {
+            return rateLimiterFactory
+                .create(callInvokable(key, ...args), rest)
+                .runOrFail(next);
+        };
+    };
+}
