eb25b9f4b797cc2b13a5d43632dd1a80b2dd7e3d
Yousif <yousif@fillerdepot.com>
2026-04-25 19:19:11 +0200

    Added circuitBreakerMiddlewareFactory

---
+++ src/circuit-breaker/implementations/middlewares/circuit-breaker-middleware-factory.ts
@@ -0,0 +1,51 @@
+/**
+ * @module RateLimiter
+ */
+
+import {
+    type CircuitBreakerTrigger,
+    type ICircuitBreakerFactoryBase,
+} from "@/circuit-breaker/contracts/_module.js";
+import { type MiddlewareFn } from "@/middleware/types.js";
+import { type ITimeSpan } from "@/time-span/contracts/_module.js";
+import {
+    callInvokable,
+    type ErrorPolicy,
+    type Invokable,
+} from "@/utilities/_module.js";
+
+/**
+ * @group Middleware
+ */
+export type CircuitBreakerMiddlewareSettings<
+    TParameters extends Array<unknown> = Array<unknown>,
+> = ErrorPolicy & {
+    /**
+     * @default
+     * ```ts
+     * (...args) => JSON.stringify(args)
+     * ```
+     */
+    key?: Invokable<TParameters, string>;
+    trigger?: CircuitBreakerTrigger;
+    slowCallTime?: ITimeSpan;
+};
+
+/**
+ * IMPORT_PATH: `"@daiso-tech/core/circuit-breaker/middlewares"`
+ * @group Middleware
+ */
+export function circuitBreakerMiddlewareFactory(
+    circuitBreakerFactory: ICircuitBreakerFactoryBase,
+) {
+    return <TParameters extends Array<unknown>, TReturn>(
+        settings: CircuitBreakerMiddlewareSettings<TParameters>,
+    ): MiddlewareFn<TParameters, Promise<TReturn>> => {
+        const { key = (...args) => JSON.stringify(args), ...rest } = settings;
+        return ({ next, args }) => {
+            return circuitBreakerFactory
+                .create(callInvokable(key, ...args), rest)
+                .runOrFail(next);
+        };
+    };
+}
