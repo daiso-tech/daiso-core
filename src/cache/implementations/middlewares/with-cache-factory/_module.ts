4115fcf0f043bd25a6adfc351815d4817d41f743
Yousif Abdulkarim <yousif.khalil.abdulkarim.1999@gmail.com>
2026-09-06 00:33:24 +0200

    Rename all cache middleware folders to use factory suffix

---
+++ src/cache/implementations/middlewares/with-cache-factory/_module.ts
@@ -0,0 +1,1 @@
+export * from "@/cache/implementations/middlewares/with-cache-factory/with-cache-factory.js";
--- src/cache/implementations/middlewares/with-cache/_module.ts
+++
@@ -1,1 +0,0 @@
-export * from "@/cache/implementations/middlewares/with-cache/with-cache-factory.js";
