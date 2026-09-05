a815d417e5242ae260424172b06be856aad35a83
Yousif Abdulkarim <yousif.khalil.abdulkarim.1999@gmail.com>
2026-09-06 01:01:23 +0200

    Removed unused eslint disable rules

--- src/lock/implementations/middlewares/with-lock-factory/with-lock-factory.test.ts
+++ src/lock/implementations/middlewares/with-lock-factory/with-lock-factory.test.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { beforeEach, describe, expect, test, vi } from "vitest";
 
 import { NoOpLockAdapter } from "@/lock/implementations/adapters/_module.js";
